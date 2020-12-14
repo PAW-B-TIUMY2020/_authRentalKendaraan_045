f9c8624c20ef0987d0621fd7804bb0f6879d409b
RafidFahar <62782723+RafidFahar@users.noreply.github.com>
2020-12-14 08:05:47 +0700

    Add Role di App

--- RentalKendaraan_045/Controllers/PeminjamenController.cs
+++ RentalKendaraan_045/Controllers/PeminjamenController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RentalKendaraan_045.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RentalKendaraan_045.Controllers
 {
@@ -18,6 +19,7 @@ namespace RentalKendaraan_045.Controllers
             _context = context;
         }
 
+        [Authorize(Policy = "readonlypolicy")]
         // GET: Peminjamen
         public async Task<IActionResult> Index(string ktsd, string searchString, string sortOrder, string currentFilter, int? pageNumber)
         {
@@ -114,6 +116,7 @@ namespace RentalKendaraan_045.Controllers
             return View(peminjaman);
         }
 
+        [Authorize(Policy = "writepolicy")]
         // GET: Peminjamen/Create
         public IActionResult Create()
         {
@@ -142,6 +145,7 @@ namespace RentalKendaraan_045.Controllers
             return View(peminjaman);
         }
 
+        [Authorize(Policy = "editpolicy")]
         // GET: Peminjamen/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -199,6 +203,7 @@ namespace RentalKendaraan_045.Controllers
             return View(peminjaman);
         }
 
+        [Authorize(Policy = "deletepolicy")]
         // GET: Peminjamen/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
